d8f3cf6060e7d4b36cbf341e08778626ab4f5f24
farulwananda <farulahmad2002@gmail.com>
2022-05-19 10:15:56 +0700

    Initial commit

---
+++ app/Http/Controllers/ApiPendidikanController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pendidikan;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Response;
+
+class ApiPendidikanController extends Controller
+{
+    public function getAll() {
+        $pendidikan = Pendidikan::all();
+        return Response::json($pendidikan, 201);
+    }
+
+    public function getPen($id) {
+        $pendidikan = Pendidikan::find($id);
+        return Response::json($pendidikan, 200);
+    }
+
+    public function createPen(Request $request) {
+        Pendidikan::create($request->all());
+
+        return response()->json([
+            'status' => 'ok',
+            'message' => 'Pendidikan berhasil ditambahkan!'
+        ], 201);
+    }
+
+    public function updatePen($id, Request $request) {
+        Pendidikan::find($id)->update($request->all());
+        return response()->json([
+            'status' => 'ok',
+            'message' => 'Pendidikan berhasil diubah!'
+        ], 201);
+    }
+
+    public function deletePen($id) {
+        Pendidikan::destroy($id);
+
+        return response()->json([
+            'status' => 'ok',
+            'message' => 'Pendidikan berhasil dihapus!'
+        ]);
+    }
+}
